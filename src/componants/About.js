6472fb88e2aa9009af27e3232f6b63025372f4dd
rishita-chouhan25 <129577017+rishita-chouhan25@users.noreply.github.com>
2024-09-05 11:49:04 +0530

    Update About.js

--- src/componants/About.js
+++ src/componants/About.js
@@ -28,13 +28,13 @@ const About = () => {
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: true }} className="section__description" data-sr-id="7">
-            Embark on a tranquil journey at our whiteFlowe Guest House, enveloped by the scenic allure of Biharinath Hill, Baranti Hill, Susunia Hill, Joychandi Hill, Garhpanchkot, Baranti Dam, Maithon Dam, and the captivating Panchat Dam. Revel in the embrace of comfort, relish delightful meals, and unwind in our verdant garden oasis. Your ideal retreat beckons, promising a harmonious blend of nature's beauty and heartfelt hospitality. Explore the hidden gems of Purulia, creating memories that linger long after your stay. <br />
+            Embark on a tranquil journey at our whiteFlower Guest House, enveloped by the scenic allure of Biharinath Hill, Baranti Hill, Susunia Hill, Joychandi Hill, Garhpanchkot, Baranti Dam, Maithon Dam, and the captivating Panchat Dam. Revel in the embrace of comfort, relish delightful meals, and unwind in our verdant garden oasis. Your ideal retreat beckons, promising a harmonious blend of nature's beauty and heartfelt hospitality. Explore the hidden gems of Purulia, creating memories that linger long after your stay. <br />
           </motion.p><h4><a href="https://maps.app.goo.gl/7wYUEB4tvR7NMHbcA">Address: xyzz....</a></h4>
-          <h4><a href="tel:+919007062180"> Contact us: +91 9329717846 </a></h4>
+          <h4><a href="tel:+919329717846"> Contact us: +91 9329717846 </a></h4>
           <br />
           <p></p>
           <div className="about__btn" data-sr-id="8">
-            <a href="https://wa.link/at5ion">
+            <a href="https://api.whatsapp.com/send?phone=919329717846">
               <motion.button variants={fadeIn("up", 0.8)}
           initial="hidden"
           whileInView={"show"}
